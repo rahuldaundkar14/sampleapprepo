8b06ca0e8e7922eca89596411752fccd30b440ec
Rahul Daundkar <rahulda@cybage.com>
2019-04-01 14:59:20 +0530

    cc

--- src/main/java/com/pages/MyPage.java
+++ src/main/java/com/pages/MyPage.java
@@ -21,15 +21,12 @@ public class MyPage {
 	public void initilise() {
 		System.out.println(System.getProperty("user.dir"));
 		System.out.println("Initilising drivers...");
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/driver/chromedriver_38");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/driver/chromedriver_40");
 
 ChromeOptions options = new ChromeOptions();
-options.addArguments("start-maximized"); // open Browser in maximized mode
-options.addArguments("disable-infobars"); // disabling infobars
-options.addArguments("--disable-extensions"); // disabling extensions
-options.addArguments("--disable-gpu"); // applicable to windows os only
-options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-options.addArguments("--no-sandbox"); // Bypass OS security model
+
+        options.addArguments("headless");
+        options.addArguments("window-size=1200x600");
 
 		driver = new ChromeDriver(options);
 		driver.get("http://google.com");
